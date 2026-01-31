1034ffb2a84510d4f52f644d5363ba20a592d069
Somit Meshram <somitmeshram3256@gmail.com>
2026-01-31 12:45:49 +0530

    Time: 42 ms (43.44%), Space: 19.3 MB (15.2%) - LeetHub

---
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
@@ -0,0 +1,20 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        l,r = 1,n
+        while l <= r:
+            m = (l+r)//2
+            result = guess(m)
+            if result == 0:
+                return m
+            elif result == 1:
+                l = m + 1
+            elif result == -1:
+                r = m - 1
+        return n

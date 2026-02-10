1b41b82646fe4aa7104d73d21ebd972a2117d3f7
Somit Meshram <somitmeshram3256@gmail.com>
2026-02-10 20:28:55 +0530

    Time: 3 ms (25.65%), Space: 19.5 MB (20.57%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,5 +1,23 @@
+#Implementing Trie Solution
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_last_word = False
+
 class Solution:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self,word) -> None:
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+        node.is_last_word = True
+
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        '''
         # If list is empty
         if not strs:
             return ""
@@ -15,3 +33,19 @@ class Solution:
                     return res[:i]
 
         return res      
+        '''
+        
+        if not strs:
+            return ""
+
+        for s in strs:
+            self.insert(s)
+
+        prefix = []
+        node = self.root
+        while len(node.children) == 1 and not node.is_last_word:
+            char , next_node  =  next(iter(node.children.items()))
+            prefix.append(char)
+            node = next_node
+
+        return "".join(prefix)

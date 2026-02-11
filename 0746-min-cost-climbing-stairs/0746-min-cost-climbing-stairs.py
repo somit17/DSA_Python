e1af18417dbd979e2afc7444c5fd6b02447bf746
Somit Meshram <somitmeshram3256@gmail.com>
2026-02-11 21:06:48 +0530

    Time: 8 ms (8.87%), Space: 21.9 MB (5.8%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -14,4 +14,18 @@ class Solution:
                 prev_1 = current
 
             return min(prev_1,prev_2)
-        return bottom_up(cost)
+
+        #Top Down Approach
+        memo = {}
+        def top_down(n)->int:
+            if n == 0:return cost[0]
+            if n == 1:return cost[1]
+            if n in memo:
+                return memo[n]
+            
+            result = cost[n] + min(top_down(n-1),top_down(n-2))
+            memo[n] = result
+            return result
+        n = len(cost)
+        return min(top_down(n-1),top_down(n-2))
+        #return bottom_up(cost)

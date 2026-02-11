3d02945eca9e5f222bf7084c2c5f2c73ee4b7197
Somit Meshram <somitmeshram3256@gmail.com>
2026-02-11 20:03:58 +0530

    Time: 0 ms (100%), Space: 19.6 MB (10.97%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -12,4 +12,20 @@ class Solution:
             result = top_down(n-1)+top_down(n-2)+top_down(n-3)
             memo[n]=result
             return memo[n]
-        return top_down(n)
+
+        #Bottom-Up
+        def bottom_up(n) -> int:
+            if n == 0: return 0
+            if n == 1: return 1
+            if n == 2: return 1
+            result = [0] * (n+1)
+            result[0] = 0
+            result[1] = 1
+            result[2] = 1
+
+            for i in range(3,n+1):
+                result[i] = result[i-1]+result[i-2]+result[i-3]
+
+            return result[n]
+
+        return bottom_up(n)
